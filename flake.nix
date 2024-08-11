9bd7eb9b31c5c74e2e24c6aea58f746e996dba1e
Anish Sevekari <anish.sevekari@gmail.com>
2024-08-11 10:37:57 -0400

    installing wayland on old laptop

--- flake.nix
+++ flake.nix
@@ -10,44 +10,14 @@
     self,
     nixpkgs,
     nixpkgs-unstable,
-    ...
   }: let
     lib = nixpkgs.lib.extend (final: prev: {
       myutils = import ./lib {lib = final;};
     });
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      config.allowUnfree = true;
-      overlays = [
-        (final: prev: {
-          unstable = import nixpkgs-unstable {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-        })
-      ];
-    };
   in {
     # output local functions defined in utils
     lib = lib.myutils;
 
     # nixos configurations for machines
-    # nixosConfigurations = {
-    #   geralt = lib.nixosSystem {
-    #     # name = "geralt";
-    #     # wsl = false;
-    #     # stateVersion = "23.05";
-    #     system = "x86_64-linux";
-    #     specialArgs = { inherit lib pkgs;};
-    #     modules = [
-    #       ./hardware-configuration.nix
-    #       ./default.nix
-    #       ./hosts/geralt.nix
-    #       ./users/stranger.nix
-    #       ./old/defaults.nix
-    #       ./old/desktop.nix
-    #     ];
-    #   };
-    # };
   };
 }
