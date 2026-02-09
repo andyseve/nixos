96f0182a846a67b99d573b41e73e0906611cd4e9
Anish Sevekari <stranger@geralt.localdomain>
2026-02-09 18:20:33 +0000

    bugfix: add default for isNixos

--- users/stranger.nix
+++ users/stranger.nix
@@ -1,5 +1,5 @@
 # user settings
-{ hostConfig, ... }:
+{ hostConfig, lib, ... }:
 rec {
   home = if hostConfig ? home then hostConfig.home else "/home";
   username = "stranger";
@@ -9,38 +9,33 @@ rec {
     {
       pkgs,
       isDarwin,
-      isNixos,
+      isNixos ? false,
       hostConfig,
       ...
     }:
-    {
-      users.users.${username} = {
-        description = name;
-        shell = "${pkgs.${shell}}/bin/${shell}";
-        home = if isDarwin then "/Users/${username}" else "${home}/${username}";
-        packages = [ pkgs.home-manager ];
+    lib.mkMerge [
+      {
+        users.users.${username} = {
+          description = name;
+          shell = "${pkgs.${shell}}/bin/${shell}";
+          home = if isDarwin then "/Users/${username}" else "${home}/${username}";
+          packages = [ pkgs.home-manager ];
+        };
       }
-      // (
-        if isNixos then
-          {
-            isNormalUser = true;
-            extraGroups = [ "wheel" ];
-            createHome = true;
-          }
-          else
-          { }
-      )
-      // (
-        if (hostConfig.shell.docker.enable or false) then
-          {
-            extraGroups = [ "docker" ];
-          }
-        else
-          { }
-      );
-    };
-
-  home-managerModule = false;
+      (lib.mkIf isNixos {
+        users.users.${username} = {
+          isNormalUser = true;
+          extraGroups = [ "wheel" ];
+          createHome = true;
+        };
+      })
+      (lib.mkIf (hostConfig.shell.docker.enable or false) {
+        users.users.${username} = {
+          extraGroups = [ "docker" ];
+        };
+      })
+    ];
+  home-manager-module = false;
 
   homeConfig =
     { isDarwin, ... }:
